2badec1aa44fd7af9af33864756d0aca7fde5d8a
Kradek <kradekmfc@gmail.com>
2013-07-18 21:55:47 -0400

    Camscore summary

---
+++ Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AellaData.AellaData" EnableViewState="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div>
+           <%= DateTime.Now.ToString() %> 
+        </div>
+        <div style="float:left">
+            Raw
+            <asp:GridView runat="server" ID="Camscores" AutoGenerateColumns="true" EnableViewState="false" />
+        </div>
+        <div style="float:left; margin-left:2em">
+            Grouped
+            <asp:GridView runat="server" ID="CamscoresGrouped" EnableViewState="false" />
+        </div>
+        
+        
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MFCChatClient;
+
+namespace AellaData
+{
+    public partial class AellaData : System.Web.UI.Page
+    {
+        void Page_Load(object sender, EventArgs evt)
+        {
+            var processed = false;
+            var client = new MFCClient();
+
+            client.UsersProcessed += (s, e) =>
+            {
+                processed = true;
+            };
+
+            while (!processed) { };
+
+            var rawList = client.Users.ToList();
+
+            Camscores.DataSource = rawList.Where(model => model.Value.ModelDetails.Camscore > 3000)
+             .Select(user => new { Model = user.Value.Name, CamScore = user.Value.ModelDetails.Camscore })
+             .OrderByDescending(x => x.CamScore);
+
+            Camscores.DataBind();
+
+            CamscoresGrouped.DataSource = 	rawList.Where(model => model.Value.ModelDetails.Camscore > 3000)
+		        .Select(user => new {Model=user.Value.Name, Camscore = user.Value.ModelDetails.Camscore, Modded=(Math.Truncate((decimal)user.Value.ModelDetails.Camscore / 1000))*1000})
+		        .GroupBy(k => k.Modded)
+		        .Select(x => new {Camscore=x.Key, Count = x.Count()})
+		        .OrderByDescending(y=> y.Camscore);
+
+            CamscoresGrouped.DataBind();
+
+        }
+    }
+}
